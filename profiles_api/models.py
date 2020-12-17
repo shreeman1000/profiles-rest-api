fe3400bed13875b273358b224b89832b6f06b38c
shreeman <64922271+shreeman1000@users.noreply.github.com>
2020-12-17 22:52:44 +0530

    added custom user profile model manager and migrations

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    '''Manager for user profiles'''
+    def create_user(self,email,name,password = None):
+        '''Create a new user profile'''
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        email = self.normalize_email(email)
+        user = self.model(email = email,name = name)
+
+        user.set_password(password)
+        user.save(using = self._db)
+
+        return user
+
+    def create_superuser(self,email,name,password):
+        '''create and save new super user with given details'''
+        user = self.create_user(email,name,password)
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using = self._db)
+
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    '''Databse model for users in the system'''
+    email = models.EmailField(max_length = 255,unique = True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default = True)
+    is_staff = models.BooleanField(default = False)
+
+    objects = UserProfileManager()
+    USERNAME_FIELD = 'email'
+    #USERNAME_FIELD is req to be in caps and is required to login
+    req_fields = ['name']
+
+    def get_full_name(self):
+        '''Retrieve Full Name of User'''
+        return self.name
+    def get_short_name(self):
+        '''Retrieve short Name of User'''
+        return self.name
+
+    def __str__(self):
+        '''Return string rep of our user'''
+        return self.email
